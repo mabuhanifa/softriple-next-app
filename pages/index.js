bacdb4e66fbd8e3f797325899020ae5132e4844e
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-19 23:13:27 +0600

    data fetched

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import HomePage from "@/components/HomePage";
+import { useEffect } from "react";
+import json from "../data/products.json";
 
 export default function Home() {
+  useEffect(() => {
+   const data = JSON.stringify(json);
+    console.log(JSON.parse(data));
+  }, []);
   return (
     <main>
       <HomePage />
