6d0716640409b6ab955d8bc1d29ef52066e50893
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-20 23:39:19 +0600

    buttons

--- components/Product.js
+++ components/Product.js
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { AiOutlineHeart } from "react-icons/ai";
+import { LiaShoppingBagSolid } from "react-icons/lia";
 
 export default function Product({ product }) {
   return (
@@ -16,6 +18,20 @@ export default function Product({ product }) {
       <p className="text-[16px]">
         {product.product.description.slice(0, 120)}...
       </p>
+      <div className="flex justify-between items-center my-3 ">
+        <button className="flex items-center gap-x-3 bg-gray-800 text-gray-100 py-2 px-3 rounded-md">
+          <span className="mb-1">
+            <LiaShoppingBagSolid size={20} />
+          </span>
+          <span className="font-[500]">Add To Cart</span>
+        </button>
+        <button className="flex items-center gap-x-3 border border-gray-300 text-gray-700 py-2.5 px-3 rounded-md group">
+          <span className="group-hover:text-red-500">
+            <AiOutlineHeart size={20} />
+          </span>
+          <span className="font-[500]">Add To Wishlist</span>
+        </button>
+      </div>
     </div>
   );
 }
