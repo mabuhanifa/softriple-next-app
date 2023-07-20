c557ce3dfe5d3bf525cc33028903d50b0e51a008
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-20 22:14:39 +0600

    navigation buttons

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,9 +1,14 @@
-import { CiLogin, CiSearch } from "react-icons/ci";
+import { CiSearch } from "react-icons/ci";
+
+
+import { BiUser } from "react-icons/bi";
+
+import { LiaShoppingBagSolid } from "react-icons/lia";
 
 import { AiOutlineHeart } from "react-icons/ai";
 
 import Link from "next/link";
-import { BsCart, BsChevronDown } from "react-icons/bs";
+import { BsChevronDown } from "react-icons/bs";
 
 export default function NavBar() {
   return (
@@ -22,10 +27,10 @@ export default function NavBar() {
       <div className="relative flex items-center">
         <input
           type="text"
-          className="pl-10 py-2 rounded border border-black/10"
+          className="pl-10 py-2.5 rounded border border-black/10"
           placeholder="Search"
         />
-        <span className="absolute top-2.5 left-2 text-2xl text-black/50">
+        <span className="absolute left-2 text-2xl text-black/50">
           <CiSearch />
         </span>
       </div>
@@ -33,7 +38,7 @@ export default function NavBar() {
         <ul className="flex items-center justify-around gap-x-5 text-gray-700">
           <li>
             <Link href={"/cart"}>
-              <BsCart size={20} />
+              <LiaShoppingBagSolid size={20} />
             </Link>
           </li>
           <li>
@@ -43,7 +48,7 @@ export default function NavBar() {
           </li>
           <li className="text-gray-900">
             <Link href={"/user"}>
-              <CiLogin size={20} />
+              <BiUser size={20} />
             </Link>
           </li>
           {/* <li className="text-gray-900">
--- components/Slider.js
+++ components/Slider.js
@@ -1,9 +1,11 @@
+import { HiOutlineArrowSmLeft, HiOutlineArrowSmRight } from "react-icons/hi";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
+import { useRef } from "react";
 import { useSelector } from "react-redux";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
@@ -13,38 +15,52 @@ export default function Slider() {
   const findAvg = (arr) => arr.reduce((a, b) => b.rating + a, 0) / arr.length;
 
   const topProducts = products.slice().sort((a, b) => {
-    return findAvg(a.reviews) - findAvg(b.reviews);
+    return findAvg(b.reviews) - findAvg(a.reviews);
   });
-
+  const swiperRef = useRef();
   return (
-    <>
+    <div className="relative">
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 5000,
+          delay: 3500,
           disableOnInteraction: false,
         }}
         pagination={{
           clickable: true,
         }}
-        navigation={true}
+        onBeforeInit={(swiper) => {
+          swiperRef.current = swiper;
+        }}
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        {products &&
-          products.map((product) => (
+        {topProducts &&
+          topProducts.slice(0, 10).map((product) => (
             <SwiperSlide key={product.id}>
               <div className="flex justify-center items-center">
-                <img
-                  src={product.product.images[0]}
-                  alt={product.title}
-                  className="h-80 w-80 object-cover my-10 rounded cursor-pointer"
-                />
+                <div></div>
+
+                <div>
+                  <img
+                    src={product.product.images[0]}
+                    alt={product.title}
+                    className="h-80 w-80 object-cover my-10 rounded cursor-pointer"
+                  />
+                </div>
               </div>
             </SwiperSlide>
           ))}
       </Swiper>
-    </>
+      <div className="flex justify-around items-center ">
+        <button onClick={() => swiperRef.current?.slidePrev()}>
+          <HiOutlineArrowSmLeft size={30} className="mt-1 text-2xl cursor-pointer" />
+        </button>
+        <button onClick={() => swiperRef.current?.slideNext()}>
+          <HiOutlineArrowSmRight size={30} className="mt-1 text-2xl" />
+        </button>
+      </div>
+    </div>
   );
 }
