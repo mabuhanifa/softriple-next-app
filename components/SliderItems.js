178f8d5bb94fb532faea22fa7f9a980b67d4931d
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-21 10:14:35 +0600

    view button

--- components/SliderItems.js
+++ components/SliderItems.js
@@ -1,16 +1,17 @@
-import { addToCart, addToWishList } from "@/redux/slices/productsSlice";
 import Image from "next/image";
-import { toast } from "react-hot-toast";
-import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
-import { LiaShoppingBagSolid } from "react-icons/lia";
-import { useDispatch } from "react-redux";
+import { useRouter } from "next/router";
+import { GoLinkExternal } from "react-icons/go";
 import CategoryBox from "./CategoryBox";
 import Star from "./Star";
 
 export default function SliderItems({ product }) {
   const findAvg = (arr) => arr.reduce((a, b) => b.rating + a, 0) / arr.length;
   const star = findAvg(product.reviews);
-  const dispatch = useDispatch();
+  const router = useRouter();
+  const pusher = () => {
+    router.push(`/product/${product.id}`);
+  };
+
   return (
     <div className="flex flex-col-reverse sm:flex-row gap-x-20 justify-center items-center text-gray-700 p-4">
       <div>
@@ -25,41 +26,21 @@ export default function SliderItems({ product }) {
             ))}
           </div>
         </div>
-        <div className="flex items-center justify-center my-3">
+        <div className="flex items-center justify-center my-5">
           {star &&
             [...Array(1).keys()].map((_, i) => <Star star={star} key={i} />)}
         </div>
-        <div className="flex flex-col gap-y-5 items-center my-5">
+        <div className="flex justify-center  items-center my-5">
           <button
-            className="flex items-center gap-x-3 bg-gray-800 hover:bg-black text-gray-100
-           py-3 px-4 active:scale-95 rounded-md"
-            onClick={() => {
-              dispatch(addToCart(product.product));
-              toast.success("Product Added To Cart");
-            }}
+            className="flex justify-center items-center gap-x-5 border border-[#cacaca] px-5 py-3 rounded-lg font-[500] hover:bg-gray-700 hover:text-white"
+            onClick={pusher}
           >
-            <span className="mb-1">
-              <LiaShoppingBagSolid size={20} />
-            </span>
-            <span className="font-[500]">Add To Cart</span>
-          </button>
-          <button
-            className="flex items-center gap-x-3 border border-gray-300 text-gray-700 
-          py-2.5 px-4 active:scale-95 rounded-md group hover:border-gray-400"
-            onClick={() => {
-              dispatch(addToWishList(product.product));
-              toast.success("Product Added To Wishlist");
-            }}
-          >
-            <span className="group-hover:text-red-500">
-              <AiOutlineHeart size={20} className="group-hover:hidden" />
-              <AiFillHeart size={20} className="hidden group-hover:block" />
-            </span>
-            <span className="font-[500]">Add To Wishlist</span>
+            <span>View</span>
+            <GoLinkExternal />
           </button>
         </div>
       </div>
-      <div className="flex justify-center items-center mb-3">
+      <div className="flex justify-center items-center mb-5">
         <Image
           src={product.product.images[0]}
           height={320}
