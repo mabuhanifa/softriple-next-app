1a9bfa993d05585541990df868b790e8dd7835d9
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-21 11:32:27 +0600

    summery checkout

--- pages/cart.js
+++ pages/cart.js
@@ -15,7 +15,7 @@ export default function Cart() {
       <h1 className="text-3xl font-bold text-gray-700 text-center mt-20">
         Shopping Cart
       </h1>
-      <div className="grid grid-cols-3 gap-5">
+      <div className="grid grid-cols-3 gap-x-10">
         <div className="col-span-2">
           <h1 className="text-md font-bold text-gray-700">Cart Items</h1>
           <div>
@@ -86,9 +86,29 @@ export default function Cart() {
             ))}
           </div>
         </div>
-        <div className="col-span-1">
-          <h1>Summery</h1>
-          <div></div>
+        <div className="col-span-1  w-[350px]">
+          <h1 className="font-bold text-gray-700 mb-5">Summery</h1>
+          <div className="flex flex-col p-5 rounded-lg bg-black/[0.05]">
+            <div className="flex items-center justify-between">
+              <p>SUBTOTAL</p>
+              <p>
+                $ {cart
+                  .reduce(
+                    (acc, item) =>
+                      acc +
+                      item.quantity * Number(item.product.price.split("$")[1]),
+                    0
+                  )
+                  .toFixed(2)}
+              </p>
+            </div>
+            <div className="text-sm md:text-md py-5 border-t border-gray-300 mt-5 leading-7">
+              The subtotal reflects the total price of your order, including
+              duties and taxes, before any applicable discounts. It does not
+              include delivery costs and international transaction fees.
+            </div>
+          </div>
+          <button className="bg-black text-white p-2.5 rounded-full mt-5 w-[350px]">Checkout</button>
         </div>
       </div>
     </div>
